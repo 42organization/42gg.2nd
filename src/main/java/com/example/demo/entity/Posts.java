7aa4b647c5b500ba134028b5e1ff40cd917f5a6f
yunji118 <yunji118@naver.com>
2023-01-06 13:37:33 +0900

    [feat]게시판 entity구현 close #3

---
+++ src/main/java/com/example/demo/entity/Posts.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id   //테이블의 pk역할
+    @GeneratedValue(strategy = GenerationType.IDENTITY)    //기본키 생성을 데이터베이스에 위임(데이터베이스에 의존적)ㄴ
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    @Column
+    private String author;
+}
