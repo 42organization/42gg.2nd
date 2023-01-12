ed24ff14c0a277944e7fc351251a7fea83fbc069
greatSweetMango <jhkim0097@gmail.com>
2023-01-12 18:20:15 +0900

    first commit

--- src/main/java/com/example/demo/controller/BoardController.java
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -1,13 +1,57 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Board;
+import com.example.demo.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class BoardController {
+
+	@Autowired
+	private BoardService boardservice;
 	
-	@GetMapping("/")
+	@GetMapping("/board/writeform")
 	public String boardWriteForm() {
-		return "boardwrite";
+		return "boardwriteform";
+	}
+
+	@GetMapping("/board/list")
+	public List<Board> BoardView() {
+		System.out.println("/board/list GET 요청");
+		return boardservice.boardList();
+	}
+
+
+	@GetMapping("/board/{id}/view")
+	public Board boardView(@PathVariable Integer id) {
+		return boardservice.boardView(id);
+	}
+	@PostMapping("/board/write")
+	public String boardWrite(Board board){
+		System.out.println("boardWrite요청");
+		boardservice.write(board);
+		return "";
+	}
+
+	@DeleteMapping("board/2/delete")
+	public String boardDelete(Integer id) {
+		boardservice.boardDelete(id);
+
+		return "";
+	}
+	
+	@PutMapping("board/{id}/update")
+	public String boardUpdate(@PathVariable Integer id,
+							  	Board board) {
+		Board boardTemp = boardservice.boardView(id);
+		boardTemp.setTitle(board.getTitle());
+		boardTemp.setContent(board.getContent());
+
+		boardservice.write(boardTemp);
+		return "";
 	}
 }
---
+++ src/main/java/com/example/demo/entity/Board.java
@@ -0,0 +1,23 @@
+package com.example.demo.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 15, nullable = true)
+    private String title;
+
+    @Column(length = 500, nullable = true)
+    private String content;
+
+}
